880da83faf82e4f420c6610f211b68b25d3d7402
germanduy@gmail.com <germanduy@gmail.com>
2023-07-20 15:17:07 +0700

    update

--- cypress/e2e/3-general/danhsachvienphi.cy.js
+++ cypress/e2e/3-general/danhsachvienphi.cy.js
@@ -11,7 +11,16 @@ describe("Viện phí", () => {
     });
     it('Danh sách viện phí', () => {
         cy.get('#txtTimKiem').type("2300520759");
+        //common.enterSelectBoxElas('drpSelectKhoaPhong','Khoa Dinh Dưỡng');
+        common.enterSelectBoxElas('drpSelectHangDoi','Thanh toán 1C');
+        common.enterSelectBoxNormal('cbbLoai','Trong tháng');
+        common.enterSelectBoxElas('drpSelectNguoiLap','sys.admin.hieutt');
+        common.enterSelectBoxElas('drpSelectDoiTuong','Bảo Hiểm');
+        //common.enterSelectBoxElas('drpSelectHopDong','01/HĐCG');
+        common.enterSelectBoxNormal('drpSelectLoaiDieuTri','Khám bệnh');
+        common.enterSelectBoxNormal('drpSelectTrangThai','Hoàn tất');
         cy.get('#btnTimKiem').click();
+        cy.get('#divVienPhiDanhSachContent tbody tr:first  td a').eq(4).click();
     });
 
    
