5b9614d050872d24e7fcb1d4f700be97fe14c74d
truonglx <truonglx8993@gmail.com>
2023-07-31 11:09:46 +0700

    update

--- cypress/e2e/3-general/khambenh.cy.js
+++ cypress/e2e/3-general/khambenh.cy.js
@@ -15,7 +15,7 @@ describe("Khám bệnh", () => {
             common.enterSelectBoxNormal('cbbLoai', testCase.cbbLoai);
             cy.get('#drpSelectTrangThai').select('2', {force: true});
             cy.get('#btnTimKiem').click();
-            // cy.get('#divKhamBenhDanhSachContent tbody tr').its('length').should('be.greaterThan', 5);
+            cy.get('#divKhamBenhDanhSachContent tbody tr').its('length').should('be.greaterThan', 5);
             cy.get('#divKhamBenhDanhSachContent tbody tr:first  td a').eq(4).click()
             common.enterSelectBoxElas('cbbBacSi', 'BM002');
             common.enterSelectBoxElas('cboChanDoanPhanBiet', 'p59.20');
