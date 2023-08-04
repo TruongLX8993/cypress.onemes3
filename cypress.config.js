ff40803ed9207a073ea2cacc81d964ab2c4f2401
admin <trongtu28032001@gmail.com>
2023-08-04 16:09:16 +0700

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
     baseUrl: 'http://192.168.1.11:2026',
     video: false
